d604610415897237f5f5a0a3d675e88f29349e57
taupoali <alistair@hodei.co.uk>
2025-02-17 10:15:31 +0000

    simple pytest

---
+++ src/mathfuncs.py
@@ -0,0 +1,5 @@
+def addnums(a,b):
+    return a + b
+
+def subnums(a, b):
+    return a - b
---
+++ src/test_mathfuncs.py
@@ -0,0 +1,7 @@
+import pytest
+from mathfuncs import addnums
+
+def test_add():
+    assert addnums(1,2) == 3
+    assert addnums(2,2) == 4
+    assert addnums(10,2000) == 2010
